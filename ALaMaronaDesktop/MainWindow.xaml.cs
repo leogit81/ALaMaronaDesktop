48f7775a97af76e525fa0ed03e178b373041ff9c
Leo AWS WinServer2012R2 <leojava2007@gmail.com>
2016-11-27 02:00:36 -0300

    se agrega capa DAL y de Domain

---
+++ ALaMaronaDesktop/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using NHibernate.Cfg;
+using System;
+using System.Windows;
+
+namespace ALaMaronaDesktop
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var nhConfig = new Configuration().Configure();
+            var sessionFactory = nhConfig.BuildSessionFactory();
+            lblTest.Content = "NHibernate configured!";
+        }
+    }
+}
---
+++ ConfigByAppConfig/Program.cs
@@ -0,0 +1,16 @@
+﻿using NHibernate.Cfg;
+using System;
+
+namespace ConfigByAppConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nhConfig = new Configuration().Configure();
+            var sessionFactory = nhConfig.BuildSessionFactory();
+            Console.WriteLine("NHibernate configured!");
+            Console.ReadKey();
+        }
+    }
+}
